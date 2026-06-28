7ae42a95dc2ef4ed661f46393f6aa97b7f90bd0d
Shiyang MENG <86827262+memolaze@users.noreply.github.com>
2026-06-29 00:57:16 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,7 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
-  base: "/onecardsau/",
+  base: "/",
   
   plugins,
   resolve: {
